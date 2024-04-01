df1579d3923f7cde777161699a6ad5c8cb80f590
alonhelvits <42146331+alonhelvits@users.noreply.github.com>
2024-04-01 11:30:23 +0300

    Another test

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     print("Hi Alon its me ,I'm using gitub")
+    print("I hate Git")
 
 
 # Press the green button in the gutter to run the script.
